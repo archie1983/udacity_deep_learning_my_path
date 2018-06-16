ced90326b0b7c885a9664398eddf75a265e5d0b2
Archie1983 <archie1983@hotmail.co.uk>
2018-06-16 22:28:12 +0100

    Implemented softmax

--- code_experiments/softmax.py
+++ code_experiments/softmax.py
@@ -3,4 +3,14 @@ import numpy as np
 # Write a function that takes as input a list of numbers, and returns
 # the list of values given by the softmax function.
 def softmax(L):
-    pass
+    import math as m
+    denominator = 0.0
+    result_list = []
+
+    for i in range(len(L)):
+        denominator += m.exp(L[i])
+
+    for i in range(len(L)):
+        result_list.append(m.exp(L[i]) / denominator)
+
+    return result_list
