b77c5f35cc4b7033b91139a49e25deaf5f42deb4
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-06-18 19:07:47 +0100

    Added some comments on why we needed to rotate input vector at the end

--- code_experiments/Gradient_descent/backprop.py
+++ code_experiments/Gradient_descent/backprop.py
@@ -41,6 +41,10 @@ delta_w_h_o = learnrate * output_error_term * hidden_layer_output
 # TODO: Calculate change in weights for input layer to hidden layer
 print ("het=", hidden_error_term)
 print ("x=", x)
+### effectively turning the x row array into a column array (almost like matrix transposition, but with just 1 row)
+### and then we can multiply the hidden_error_term row vector with x, which is now a column vector.
+### Just so happened that they both ended up being row vectors, but we can only multiply them as row x col vectors.
+### And this is ordinary multiplication, so it will just be each element multiplied by each other element.
 delta_w_i_h = learnrate * hidden_error_term * x[:, None]
 
 print('Change in weights for hidden layer to output layer:')
