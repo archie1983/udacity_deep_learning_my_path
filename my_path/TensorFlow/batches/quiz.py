ff0899b619362f13b13a4c7bb23f7a04e61c583d
Archie1983 <archie1983@hotmail.co.uk>
2018-07-01 00:10:19 +0100

    Mini batching quiz complete

--- my_path/TensorFlow/batches/quiz.py
+++ my_path/TensorFlow/batches/quiz.py
@@ -9,5 +9,21 @@ def batches(batch_size, features, labels):
     """
     assert len(features) == len(labels)
     # TODO: Implement batching
-    pass
+    cur_features = []
+    cur_labels = []
+    result = []
 
+    for i in range(len(features)):
+        cur_features.append(features[i])
+        cur_labels.append(labels[i])
+        if ((i + 1) % batch_size) == 0:
+            result.append([cur_features, cur_labels])
+            cur_features = []
+            cur_labels = []
+    
+    ### If anything is left over, then adding that too. We'll not that there are lelftovers if 
+    ### there is a remainder of division of number of features with batch size
+    if (len(features) % batch_size) != 0:
+        result.append([cur_features, cur_labels])
+
+    return result
