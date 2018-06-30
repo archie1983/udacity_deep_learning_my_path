77ab1df22218dba6415e72a68389e636a42ebe1f
Archie1983 <archie1983@hotmail.co.uk>
2018-07-01 00:21:30 +0100

    Added an alternative solution

--- my_path/TensorFlow/batches/quiz.py
+++ my_path/TensorFlow/batches/quiz.py
@@ -27,3 +27,15 @@ def batches(batch_size, features, labels):
         result.append([cur_features, cur_labels])
 
     return result
+
+
+### Alternative solution:
+#    output_batches = []
+#    
+#    sample_size = len(features)
+#    for start_i in range(0, sample_size, batch_size):
+#        end_i = start_i + batch_size
+#        batch = [features[start_i:end_i], labels[start_i:end_i]]
+#        output_batches.append(batch)
+#        
+#    return output_batches
