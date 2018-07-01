e3cb9b836ca5e077a0183b5297d9c6c5777221b8
Archie1983 <archie1983@hotmail.co.uk>
2018-07-02 00:12:48 +0100

    TensorFlow ReLUs quiz finished

--- my_path/TensorFlow/ReLUS/quiz.py
+++ my_path/TensorFlow/ReLUS/quiz.py
@@ -24,6 +24,25 @@ biases = [
 features = tf.Variable([[1.0, 2.0, 3.0, 4.0], [-1.0, -2.0, -3.0, -4.0], [11.0, 12.0, 13.0, 14.0]])
 
 # TODO: Create Model
+#w0 = tf.placeholder(tf.float32)
+#w1 = tf.placeholder(tf.float32)
+#b0 = tf.placeholder(tf.float32)
+#b1 = tf.placeholder(tf.float32)
+#f = tf.placeholder(tf.float32)
+
+#hidden_layer = tf.add(tf.matmul(f, w0), b0)
+#hidden_layer = tf.nn.relu(hidden_layer)
+
+#output = tf.add(tf.matmul(hidden_layer, w1), b1)
+
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+
+output = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
 
 # TODO: Print session results
-
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    #sess.run(output, feed_dict={w0: weights[0], b0: biases[0], w1: weights[1], b1: biases[1], f: features})
+    result = sess.run(output)
+    print(result)
