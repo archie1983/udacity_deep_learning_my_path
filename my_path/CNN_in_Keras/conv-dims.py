0f7c8b810bfb570f4a65d039751c498da983cef4
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-07-05 13:45:45 +0100

    Added some explanation about shape of convolutional layer to the layer example

--- my_path/CNN_in_Keras/conv-dims.py
+++ my_path/CNN_in_Keras/conv-dims.py
@@ -9,3 +9,25 @@ model.summary()
 ### Number of parameters is: kernel_size_h * kernel_size_w * filters * depth + filters
 ### depth here is 1. Filters are added in the end, because they represent how many biases
 ### we will have (1 per filter)
+
+
+### Formula: Shape of a Convolutional Layer
+### The shape of a convolutional layer depends on the supplied values of kernel_size, input_shape, padding, and stride. Let's define a few variables:
+
+### K - the number of filters in the convolutional layer
+### F - the height and width of the convolutional filters
+### S - the stride of the convolution
+### H_in - the height of the previous layer
+### W_in - the width of the previous layer
+### Notice that K = filters, F = kernel_size, and S = stride. Likewise, H_in and W_in are the first and second value of the input_shape tuple, respectively.
+
+### The depth of the convolutional layer will always equal the number of filters K.
+
+### If padding = 'same', then the spatial dimensions of the convolutional layer are the following:
+
+### height = ceil(float(H_in) / float(S))
+### width = ceil(float(W_in) / float(S))
+### If padding = 'valid', then the spatial dimensions of the convolutional layer are the following:
+
+### height = ceil(float(H_in - F + 1) / float(S))
+### width = ceil(float(W_in - F + 1) / float(S))
