86cf1e75d69fef3d510697525a2e8bcfe97cc282
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-07-05 13:43:04 +0100

    Changed parameters of convolutional layer for the quiz

--- my_path/CNN_in_Keras/conv-dims.py
+++ my_path/CNN_in_Keras/conv-dims.py
@@ -2,6 +2,10 @@ from keras.models import Sequential
 from keras.layers import Conv2D
 
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=2, strides=2, padding='valid', 
-    activation='relu', input_shape=(200, 200, 1)))
+model.add(Conv2D(filters=32, kernel_size=3, strides=2, padding='same', 
+    activation='relu', input_shape=(128, 128, 3)))
 model.summary()
+
+### Number of parameters is: kernel_size_h * kernel_size_w * filters * depth + filters
+### depth here is 1. Filters are added in the end, because they represent how many biases
+### we will have (1 per filter)
