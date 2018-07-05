26793eb0d7572bf33ce1c56cd61a1d934bc6608e
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-07-05 15:39:20 +0100

    Experimented with parameters for the pooling layer, which changed resulting output shape.

--- my_path/CNN_in_Keras/pool-dims.py
+++ my_path/CNN_in_Keras/pool-dims.py
@@ -2,5 +2,5 @@ from keras.models import Sequential
 from keras.layers import MaxPooling2D
 
 model = Sequential()
-model.add(MaxPooling2D(pool_size=2, strides=2, input_shape=(100, 100, 15)))
+model.add(MaxPooling2D(pool_size=(2, 3), strides=(2, 3), input_shape=(100, 100, 15)))
 model.summary()
