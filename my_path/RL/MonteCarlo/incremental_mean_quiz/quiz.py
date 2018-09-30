c0d1735b3d81b79b529ee6e23218527f27fa6c69
Archie1983 <archie1983@hotmail.co.uk>
2018-09-30 18:14:24 +0100

    Running Incremental mean quiz solved

--- my_path/RL/MonteCarlo/incremental_mean_quiz/quiz.py
+++ my_path/RL/MonteCarlo/incremental_mean_quiz/quiz.py
@@ -5,6 +5,10 @@ def running_mean(x):
     mean_values = []
     for k in np.arange(0, len(x)):
         # TODO: fill in the update step
-        mu = ...
+        if k == 0:
+            mu = x[k]
+        else:
+            mu = mu + (1/(k + 1)) * (x[k] - mu)
+
         mean_values.append(mu)
     return mean_values
