1afb702ca04105cf7ad202ed75608d7d6275b158
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-11-01 15:25:44 +0000

    Starting MSE node

--- my_path/MiniFlow/implementation/miniflow.py
+++ my_path/MiniFlow/implementation/miniflow.py
@@ -106,7 +106,7 @@ class Sigmoid(Node):
 
         Your code here!
         """
-        return 1 / (1 + np.exp(-x))
+        return 1. / (1. + np.exp(-x)) # gotta ensure that 1. is treated as a foat
 
     def forward(self):
         """
@@ -120,6 +120,35 @@ class Sigmoid(Node):
         #self.value = -1
         inbound_value = self.inbound_nodes[0].value # should be just one node inbound
         self.value = self._sigmoid(inbound_value)
+
+
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+        # TODO: your code here
+        pass
+
 
 """
 No need to change anything below here!
