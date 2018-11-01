3b51b1d9277bb4d2c62404a9a300116c758e7d6b
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-11-01 13:09:57 +0000

    Build a linear neuron that generates an output by applying a simplified version of the weighted sum. Linear should take a list of inbound nodes of length n, a list of weights of length n, and a bias.

--- my_path/MiniFlow/implementation/miniflow.py
+++ my_path/MiniFlow/implementation/miniflow.py
@@ -1,6 +1,4 @@
-"""
-You need to change the Add() class below.
-"""
+import numpy as np
 
 class Node(object):
     def __init__(self, inbound_nodes=[]):
@@ -61,6 +59,29 @@ class Add(Node):
         self.value = 0
         for node in self.inbound_nodes:
             self.value += node.value
+
+
+class Linear(Node):
+    def __init__(self, inputs, weights, bias):
+        Node.__init__(self, [inputs, weights, bias])
+
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other nodes.
+        # The weight and bias values are stored within the
+        # respective nodes.
+
+    def forward(self):
+        """
+        Set self.value to the value of the linear function output.
+
+        Your code goes here!
+        """
+        inputs = self.inbound_nodes[0].value
+        weights = self.inbound_nodes[1].value
+        bias = self.inbound_nodes[2].value
+        self.value = np.dot(inputs, weights) + bias
+
+
 """
 No need to change anything below here!
 """
