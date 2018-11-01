63901f2dd5104528d3b614399d6e2d44116408dc
Arturs Elksnis <arturs.elksnis@ggtg.net>
2018-11-01 12:07:26 +0000

    The other method at your disposal is forward_pass(), which actually runs the network and outputs a value.

--- my_path/MiniFlow/topological_sort_example.py
+++ my_path/MiniFlow/topological_sort_example.py
@@ -10,4 +10,19 @@ feed_dict = {x: 10, y: 20}
 # Sort the nodes with topological sort.
 sorted_nodes = topological_sort(feed_dict=feed_dict)
 
+def forward_pass(output_node, sorted_nodes):
+    """
+    Performs a forward pass through a list of sorted nodes.
 
+    Arguments:
+
+        `output_node`: The output node of the graph (no outgoing edges).
+        `sorted_nodes`: a topologically sorted list of nodes.
+
+    Returns the output node's value
+    """
+
+    for n in sorted_nodes:
+        n.forward()
+
+    return output_node.value
